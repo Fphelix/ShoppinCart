cba0fe9a013ce074628de579019d62b752e73d87
Fphelix <77512995+Fphelix@users.noreply.github.com>
2021-03-21 01:12:43 -0400

    ShoppingApp

---
+++ ShoppingApp/ShoppingApp/Clothing.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingApp
+{
+    class Clothing : IComparable<Clothing>
+    {
+        private string description;
+        private double price;
+        private string size;
+
+        public static double MIN_PRICE = 10.0;
+        public static double TAX_RATE = 0.2;
+
+        public Clothing()
+        {
+
+        }
+        public Clothing(string description, double price, string size)
+        {
+            setDescription(description);
+            setPrice(price);
+            setSize(size);
+
+        }
+        public string getDescription()
+        {
+            return description;
+        }
+        public void setDescription(string description)
+        {
+            this.description = description;
+        }
+
+        public string getSize()
+        {
+            return size;
+        }
+        public void setSize(string description)
+        {
+            this.size = description;
+        }
+        public double getPrice()
+        {
+            return price + (price * TAX_RATE);
+        }
+        public void setPrice(double price)
+        {
+            this.price = (price > MIN_PRICE) ? price : MIN_PRICE;
+
+        }
+
+        public override String ToString()
+        {
+            return $"item : {getDescription()}   | Size: {getSize()} | Price: {getPrice()}";
+        }
+
+
+        public int CompareTo(Clothing other)
+        {
+            return this.description.CompareTo(other.description);
+
+        }
+    }
+}
+
---
+++ ShoppingApp/ShoppingApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ShoppingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tax = 0.2;
+            double total = 0.0;
+            double avg = 0.0;
+
+            Console.WriteLine("Welcome to the Shop  ");
+
+            Customer c1 = new Customer("Pinky", 2);
+
+            Console.WriteLine($"Min Price {Clothing.MIN_PRICE}");
+
+
+            Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
+            Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
+
+
+            Clothing[] items = { item1, item2, new Clothing("Green Scarf", 5.0, "S"), new Clothing("Blue-T-Shirt", 10.5, "S") };
+
+            c1.addItem(items);
+
+            Console.WriteLine($"Customer: {c1.getName()} | Size:  {c1.getSize()} | Total Cost: {c1.getTotalClothingCost()}");
+            int count = 0;
+            double sum = 0.0;
+
+            for (int i = 0; i < c1.getItems().Length; i++)
+            {
+                Console.WriteLine($"item : {items[i]}");
+            }
+
+            for (int i = 0; i < c1.getItems().Length; i++)
+            {
+                if (items[i].getSize() == "L")
+                {
+                    count++;
+                    sum += items[i].getPrice();
+                }
+            }
+
+            try
+            {
+                avg = (count == 0) ? 0 : avg / count;
+
+                avg = sum / count;
+                Console.WriteLine($"Average Price : {avg}  {count}");
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cant diveide by 0");
+            }
+
+            Array.Sort(c1.getItems());
+        }
+    }
+}
